5ae8bd5b667b39d7a1b567445f74a4f345849f1a
Phaniraj <79626160+phani-training@users.noreply.github.com>
2022-09-12 11:01:14 +0530

    Add files via upload

---
+++ SampleWebFormApp/UserControls/TimeControl.ascx
@@ -0,0 +1,7 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="TimeControl.ascx.cs" Inherits="SampleWebFormApp.UserControls.TimeControl" %>
+<%@ OutputCache Duration="60" VaryByParam="None" %>
+<div>
+   <h2>
+       The Time is :<asp:Label Text="" runat="server" ID="lblTime" />
+   </h2>  
+</div>
---
+++ SampleWebFormApp/UserControls/TimeControl.ascx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SampleWebFormApp.UserControls
+{
+    public partial class TimeControl : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblTime.Text = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
