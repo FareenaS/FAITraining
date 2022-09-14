41c9ee11d20a1e639e5dbbbe6146e2dfd2f2ebea
Phaniraj <79626160+phani-training@users.noreply.github.com>
2022-09-14 09:55:03 +0530

    Add files via upload

--- SampleMvcApp/Controllers/AjaxController.cs
+++ SampleMvcApp/Controllers/AjaxController.cs
@@ -1,38 +1,63 @@
-﻿using System;
+﻿using SampleMvcApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using SampleMvcApp.Models;
+
 namespace SampleMvcApp.Controllers
 {
     public class AjaxController : Controller
     {
+        // GET: Ajax
         public ActionResult Home()
         {
             return View();
         }
 
-        public PartialViewResult EmpList()
+        public PartialViewResult GetAllEmployees()
         {
             var data = new RecapEntities().EmpTables.ToList();
             return PartialView(data);
         }
-
-        public PartialViewResult AddNew()
+        #region Manipulations
+        public PartialViewResult AddNewEmployee()
         {
-            var depts = new RecapEntities().DeptTables.ToList();
-            var items = depts.Select((d) => new SelectListItem { Text = d.DeptName, Value = d.DeptId.ToString() }).ToList();
-            ViewBag.Depts = items;
             return PartialView(new EmpTable());
         }
+
         [HttpPost]
-        public RedirectToRouteResult AddNew(EmpTable rec)
+        public ActionResult AddNewEmployee(EmpTable rec)
         {
             var context = new RecapEntities();
-            context.EmpTables.Add(rec);
-            context.SaveChanges();
-            return RedirectToAction("EmpList");
+            context.EmpTables.Add(rec);//Add to the collection data
+            context.SaveChanges();//Commit to the database.
+            return RedirectToAction("GetAllEmployees");
         }
+
+        public ActionResult Edit(string id)
+        {
+            var empId = Convert.ToInt32(id);
+            var context = new RecapEntities();
+            var selected = context.EmpTables.FirstOrDefault((e) => e.EmpId == empId);
+            if (selected == null) throw new Exception("Failed to get the record");
+            return PartialView(selected);
+        }
+        [HttpPost]
+        public ActionResult Edit(EmpTable updatedRec)
+        {
+            var context = new RecapEntities();
+            var selected = context.EmpTables.FirstOrDefault((e) => e.EmpId == updatedRec.EmpId);
+            if (selected == null) throw new Exception("Failed to get the record to Update");
+            selected.DeptId = updatedRec.DeptId;
+            selected.DateOfBirth = updatedRec.DateOfBirth;
+            selected.EmpAddress = updatedRec.EmpAddress;
+            selected.EmpSalary = updatedRec.EmpSalary;
+            selected.EmpName = updatedRec.EmpName;
+            context.SaveChanges();
+            return RedirectToAction("GetAllEmployees");
+        }
+
+        #endregion
     }
 }
