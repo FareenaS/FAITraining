96412c5377c56d51ca13c724a5d474b41f13e407
Phaniraj <79626160+phani-training@users.noreply.github.com>
2022-09-16 09:28:39 +0530

    Add files via upload

---
+++ SampleWebApi/CustomFunctions.js
@@ -0,0 +1,35 @@
+﻿const btnHander = (btn, handler) => btn.click(handler);
+
+function findRec(id) {
+    let tempUrl = url + '/' + id;//Append the url to pass the id as Query string. 
+    $.get(tempUrl, (data) => {
+        alert(data.EmpName)
+    })
+}
+
+function populateData(data) {
+    $("table").find("tr:gt(0)").remove();
+    $.each(data, function (index, element) {
+        let row = `<tr><td>${element.EmpID}</td><td>${element.EmpName}</td><td>${element.EmpAddress}</td><td>${element.EmpSalary}</td><td><a href="#" onClick="findRec(${element.EmpID})">Edit</td></tr>`;
+        $("table").append(row);
+    })
+}
+
+function deleteRec(id) {
+    let tempUrl = url + '/' + id;//Append the url to pass the id as Query string. 
+    $.ajax({
+        "method": "DELETE",
+        "url": tempUrl,
+        "success": (msg) => alert(msg)
+    })
+}
+
+function createEmpObject() {
+    const obj = {};
+    obj.EmpName = $("#txtName").val();
+    obj.EmpAddress = $("#txtAddress").val();
+    obj.EmpSalary = $("#txtSalary").val();
+    obj.DeptId = $("#txtDept").val();
+    obj.DateOfBirth = $("#txtDob").val();
+    return obj;
+}
